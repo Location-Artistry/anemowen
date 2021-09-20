03bd4940810e4a540a51f74201935187443cd13d
Location-Artistry <erickerney@gmail/com>
2021-09-20 14:11:39 -0400

    Work on sensor dashboard

--- stations/urls.py
+++ stations/urls.py
@@ -1,7 +1,7 @@
 from stations.forms import AddForm
 from django.urls import path
 from . import views
-from .views import AddStationView, HomeView, StationDetailView, UpdateStationView, DeleteStationView, StationDetailViewMap, HomeMapView
+from .views import AddStationView, HomeView, StationDetailView, UpdateStationView, DeleteStationView, StationDetailViewMap, HomeMapView, StationList
 
 urlpatterns = [
     # path('',views.home, name="home")
@@ -12,5 +12,8 @@ urlpatterns = [
     path('station/<int:pk>/remove', DeleteStationView.as_view(), name='delete_station'),
     path('stationmap/<int:pk>', StationDetailViewMap.as_view(), name='station-detail-map'),
     path('map/', HomeMapView.as_view(), name='home_map'),
-    path('data', views.getLiveStations, name='get_data'),      
+    path('data', views.nhbpSensorMap, name='nhbp-sensor-map'),
+    path('sensor_dashboard', views.dashboard, name='sensor-dashboard'),
+    path('list', StationList.as_view(), name='station-list' ),
+    
 ]
--- stations/views.py
+++ stations/views.py
@@ -1,4 +1,6 @@
+from abc import abstractclassmethod
 from django.http import response
+from requests.api import request
 from stations.models import Stations
 from django.shortcuts import render
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
@@ -39,6 +41,42 @@ class HomeMapView(ListView):
     model = Stations
     template_name = 'home_map.html'
 
-def getLiveStations(request):
+class StationList(ListView):
+    model = Stations
+    template_name = 'station_list.html'
+
+def StationListFunc(request):
+    model = Stations
+    queryset = Stations.objects.all()
+    return render(request, 'station_list.html', {'response': queryset })
+
+def nhbpSensorMap(request):
     response = requests.get('https://purpleairwidget.firebaseapp.com/purpleAirData/44439,41995,41993,42005,41907,97713,97553,97743,97679,91997,97559,95527,92021').json()
-    return render(request, 'get_data.html', {'response':response} )
+    return render(request, 'nhbp_sensor_map.html', {'response':response} )
+
+def dashboard(request):
+    liveStations = requests.get('https://purpleairwidget.firebaseapp.com/purpleAirData/44439,41995,41993,42005,41907,97713,97553,97743,97679,91997,97559,95527,92021').json()
+    
+    def getTime(t):
+        totSec = t['seconds'] + (t['minute'] * 60) + (t['hour'] * 3600) + (t['day'] * 86400)
+        return totSec
+
+    def getSummary(data):
+        avgPM25, avgAQI, avgSec = 0, 0, 0
+        for x in data:
+            avgPM25 = avgPM25 + x['properties']['PM2_5Value']
+            avgAQI = avgAQI + x['properties']['AQI']
+            avgSec = avgSec + getTime(x['properties']['formatSinceSeen'])
+        avgPM25 = round(avgPM25 / len(data), 2)
+        avgAQI = round(avgAQI / len(data))
+        avgSec = avgSec / len(data)
+        timeMin = round(avgSec / 60, 2)
+        return [avgPM25, avgAQI, timeMin]
+
+    summary = getSummary(liveStations['features'])
+    avgPM25, avgAQI, timeMin = summary[0], summary[1], summary[2]
+    stationData = Stations.objects.all()
+    stationCount = Stations.objects.all().count()
+    context = {'response': liveStations, 'stationData': stationData, 'stationCount': stationCount, 'avgPM25': avgPM25, 'avgAQI': avgAQI, 'timeMin': timeMin }
+    return render(request, 'sensor_dashboard.html', context)
+
