97c43d440ff6c0f4b1525066551c317a0c8f4435
Nottawaseppi <NHBPTEAC@gmail.com>
2021-10-21 15:30:48 -0400

    working

--- mnoki/settings.py
+++ mnoki/settings.py
@@ -136,7 +136,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+#import dj_database_url
+#db_from_env = dj_database_url.config(conn_max_age=500)
+#DATABASES['default'].update(db_from_env)
 
