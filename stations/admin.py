c72c997d68a5cca373b4d40cd913cc85297aa15b
Nottawaseppi <NHBPTEAC@gmail.com>
2021-10-20 23:22:34 -0400

    update requirements

--- stations/admin.py
+++ stations/admin.py
@@ -1,7 +1,6 @@
 #from django.contrib import admin
 from django.contrib.gis import admin
 from .models import Stations
-from leaflet.admin import LeafletGeoAdmin
 
 admin.site.register(Stations, admin.OSMGeoAdmin)
 #admin.site.register(Stations, LeafletGeoAdmin)
