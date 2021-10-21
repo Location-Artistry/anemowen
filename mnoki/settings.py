b1d8670936600e31b9d60fc6dde43328d487a793
Nottawaseppi <NHBPTEAC@gmail.com>
2021-10-20 23:49:52 -0400

    update requirements

--- mnoki/settings.py
+++ mnoki/settings.py
@@ -138,5 +138,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# DATABASES['default'].update(db_from_env)
 
