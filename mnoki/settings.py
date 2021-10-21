ac03f9c1da1dd5989783f8222f56f4d696d590c0
Nottawaseppi <NHBPTEAC@gmail.com>
2021-10-20 23:27:11 -0400

    update requirements

--- mnoki/settings.py
+++ mnoki/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
